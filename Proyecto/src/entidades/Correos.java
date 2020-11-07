4c2b3382aff7483ed1a88adb569f3fcc0005d2ae
MASC-09 <49210830+MASC-09@users.noreply.github.com>
2020-11-07 14:31:17 -0600

    Add files via upload

---
+++ Proyecto/src/entidades/Correos.java
@@ -0,0 +1,21 @@
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Correos
+ *
+ */
+@Entity
+
+public class Correos implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Correos() {
+		super();
+	}
+   
+}
